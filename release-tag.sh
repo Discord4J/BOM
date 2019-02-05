40bfb464b9901ce1d98ab29eebd76304e804c18e
quanticc <quanticc@users.noreply.github.com>
2019-02-04 23:45:20 -0300

    Remove release scripts

--- release-tag.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -z "$1" ]
-  then
-    echo "Version argument missing"
-    exit 1
-fi
-./gradlew -Pversion="$1" -Pdiscord4jVersion="$1" -Pdiscord4jJitpackVersion="$1" -PstoresVersion="$1" -PstoresJitpackVersion="$1" publishToMavenLocal bintrayUpload
--- release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -z "$1" ]
-  then
-    echo "Version argument missing"
-    exit 1
-fi
-./gradlew "$@" publishToMavenLocal bintrayUpload
